ee0da5b56cbc59b043a7deab5ca0e6932f9cc565
Kelly Smith <k.kmsmith.smith@gmail.com>
2018-07-18 08:16:59 -0400

    adding path constants

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 var http = require('http');
 var _ = require('lodash');
+
+const wsurl ="https://s3.amazonaws.com/challenge.getcrossbeam.com/public/";
+//const wsurl ="http://localhost:3000/api/"; /// test url
