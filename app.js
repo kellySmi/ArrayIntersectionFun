6e7aa58f15f86c90eab56d0d7d56f87ab9ab1ede
Kelly Smith <k.kmsmith.smith@gmail.com>
2018-07-18 08:25:17 -0400

    adding uniqueBy and map functions

--- app.js
+++ app.js
@@ -11,7 +11,23 @@ https.get(wsurl+ process.argv[2]+".json", (res) => {
   res.on('data', (chunk) => { rawData += chunk; });
   res.on('end', () => {
     try {
-      
+      const res1 = JSON.parse(rawData);
+      //console.log(res1);
+      let names1 =  _.uniqBy(res1.companies,r1=>r1.domain);
+      let n1Ar = names1.map(n=>n.domain);
+      https.get(wsurl+ process.argv[3]+".json", (res2) => {
+      res2.setEncoding('utf8');
+         let rawData = '';
+         res2.on('data', (chunk) => { rawData += chunk; });
+         res2.on('end', () => {
+           const res3 = JSON.parse(rawData);
+            let names2 =  _.uniqBy(res3.companies,r3=>r3.domain);
+            let n2Ar = names2.map(n=>n.domain);
+            
+         });
+       }).on('error', (e) => {
+          console.error(`Got error: ${e.message}`);
+       });
     } catch (e) {
       console.error(e.message);
     }
